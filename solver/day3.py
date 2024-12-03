9eed14340549c58ee985de93308ccb54613470df
Rohit Singh <rkochhar@uwaterloo.ca>
2024-12-03 11:04:10 -0500

    Refactor Day 3 solution

--- solver/day3.py
+++ solver/day3.py
@@ -1,20 +1,26 @@
 import re
 
-def part_a(input: str):
+def part_a(input: str, disable_instr_toggle=True):
     """
     Given a set of "corrupted instructions", i.e:
         xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
     Extract all mul(x,y) instructions, perform a multiplication operation, and sum
     """ 
     # Pull all the multiplication operations
-    matches = re.findall(r'mul\([0-9]+,[0-9]+\)', input.strip())
+    matches = re.findall(r"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)", input.strip())
     # Perform the multiplication for all matches
+    enabled = True
     total = 0
     for match in matches:
-        # Extract first and second number
-        nums = re.findall(r'\d+', match)
-        total += int(nums[0]) * int(nums[1])
-        
+        # Only let the enabled flag go down if 
+        if match == "do()":
+            enabled = disable_instr_toggle or True
+        elif match == "don't()":
+            enabled = disable_instr_toggle or False
+        else:
+            nums = re.findall(r'\d+', match)
+            total += int(nums[0]) * int(nums[1]) if enabled else 0
+
     return total
 
 def part_b(input: str):
@@ -24,18 +30,4 @@ def part_b(input: str):
     Peform the same mutliplication, but only if it is activated
     """
     # Get all valid operations
-    matches = re.findall(r"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)", input.strip())
-    
-    # Perform multiplication for all enabled matches
-    enabled = True
-    total = 0
-    for match in matches:
-        if match == "do()":
-            enabled = True
-        elif match == "don't()":
-            enabled = False
-        else:
-            nums = re.findall(r'\d+', match)
-            total += int(nums[0]) * int(nums[1]) if enabled else 0
-
-    return total
+    return part_a(input, disable_instr_toggle=False)
